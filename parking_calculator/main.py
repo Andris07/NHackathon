1b542a2abe1c3745eb94013a6619607fb69e286e
Andris <laczkovicsandris07.education@gmail.com>
2026-04-16 21:58:28 +0200

    parking_calculator methods + fix
    datetime -> str so types match
    seconds <= 60 * 30 so it stays int
    some methods that'll be used later

--- parking_calculator/main.py
+++ parking_calculator/main.py
@@ -2,17 +2,33 @@ from pathlib import Path
 
 from datetime import datetime
 
-def convert_to_total_seconds(START: datetime, END: datetime) -> int:
+def convert_to_total_seconds(START: str, END: str) -> int:
     START_date = datetime.strptime(START, "%Y-%m-%d %H:%M:%S")
     END_date = datetime.strptime(END, "%Y-%m-%d %H:%M:%S")
-    seconds = (END_date - START_date).total_seconds()
+    seconds = int((END_date - START_date).total_seconds())
 
     return seconds
 
-def parking_fee(START: datetime, END: datetime) -> int:
+def convert_to_minutes(seconds: int) -> int:
+    return seconds // 60
+
+def convert_to_hours(seconds: int) -> int:
+    return seconds // (60 * 60)
+
+def remainder(seconds: int) -> int:
+    return seconds % 60
+
+
+
+def parking_fee(START: str, END: str) -> int:
     seconds = convert_to_total_seconds(START, END)
-    
+
+    if seconds <= 60 * 30:
+        return 0
+
     return seconds
+
+
 
 def main():
     # data = Path("input.txt").read_text(encoding="utf-8")
@@ -20,7 +36,7 @@ def main():
 
     BASE_DIR = Path(__file__).resolve().parent
     data = (BASE_DIR / "input.txt").read_text(encoding="utf-8")
-    print(parking_fee("2026-03-30 07:45:12", "2026-03-30 09:10:33"))
+    print(parking_fee("2026-03-30 07:00:00", "2026-03-30 07:30:00"))
 
 if __name__ == "__main__":
     main()
