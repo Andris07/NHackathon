02450837ae1af7cdbe41d70dcc4ca55e0840a70e
Andris <laczkovicsandris07.education@gmail.com>
2026-04-16 21:40:56 +0200

    parking_calculator convert_to_total_seconds method
    Calculates the total seconds between the END and START date and returns the value. Will be helpful when we check if the log is valid (END is AFTER the START date)

--- parking_calculator/main.py
+++ parking_calculator/main.py
@@ -1,10 +1,26 @@
 from pathlib import Path
 
+from datetime import datetime
+
+def convert_to_total_seconds(START: datetime, END: datetime) -> int:
+    START_date = datetime.strptime(START, "%Y-%m-%d %H:%M:%S")
+    END_date = datetime.strptime(END, "%Y-%m-%d %H:%M:%S")
+    seconds = (END_date - START_date).total_seconds()
+
+    return seconds
+
+def parking_fee(START: datetime, END: datetime) -> int:
+    seconds = convert_to_total_seconds(START, END)
+    
+    return seconds
 
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    # data = Path("input.txt").read_text(encoding="utf-8")
+    # print(data, end="")
 
+    BASE_DIR = Path(__file__).resolve().parent
+    data = (BASE_DIR / "input.txt").read_text(encoding="utf-8")
+    print(parking_fee("2026-03-30 07:45:12", "2026-03-30 09:10:33"))
 
 if __name__ == "__main__":
     main()
