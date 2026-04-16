c7ed857dd91bf1c797a704d109fd3550220c7339
Andris <laczkovicsandris07.education@gmail.com>
2026-04-16 19:51:04 +0200

    drop_test max_height_when_it_breaks method
    Calculates the minimum height where all N device breaks, aka the first height where the test failes and cannot be continued without any new device. Uses tries and H (tower's height) as inputs

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,10 +1,27 @@
 from pathlib import Path
 
+def min_height_when_it_breaks(tries: int, H: int) -> int:
+    max_H = 0
+    step = tries
+    
+    while step > 0 and max_H < H + 1:
+        max_H += step
+        step -= 1
+
+    return max_H
+
+def min_num_of_drops(N: int, H: int) -> int:
+    return
 
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    # data = Path("input.txt").read_text(encoding="utf-8")
+    # print(data, end="")
+    BASE_DIR = Path(__file__).resolve().parent
+    data = (BASE_DIR / "input.txt").read_text(encoding="utf-8")
 
+    data = data.splitlines();
+    print("\n".join(data))
+    print(min_height_when_it_breaks(14, 100))
 
 if __name__ == "__main__":
     main()
