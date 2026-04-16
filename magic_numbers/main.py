41d052e91f56d0868c1a07e65e1a025dc1aa6ce8
Andris <laczkovicsandris07.education@gmail.com>
2026-04-16 12:00:44 +0200

    n_m_n third case check with convert function
    Checks if there's any overflow number, if yes, then it returns a new number, if no it returns the mirrored number. The Convert function makes sure all input values are handled right

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,10 +1,31 @@
 from pathlib import Path
 
+def convert(data: str) -> str:
+    total = 0
+
+    for line in data.splitlines():
+        line = line.strip()
+        if not line:
+            continue
+
+        # ha van ^, akkor hatvány
+        if "^" in line:
+            base, exp = line.split("^")
+            total += int(base) ** int(exp)
+        else:
+            # sima szám
+            digits = "".join(ch for ch in line if ch.isdigit())
+            if digits:
+                total += int(digits)
+
+    return str(total)
+
 def next_magic_num(n: str) -> str:
     s = list(n)
     length = len(s)
 
     def mirror(arr):
+        arr = arr.copy()
         l = len(arr)
         for i in range(l // 2):
             arr[l - 1 - i] = arr[i]
@@ -24,10 +45,22 @@ def next_magic_num(n: str) -> str:
         s[i] = str(new % 10)
         i -= 1
 
-def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    if carry:
+        return "1" + "0" * (length - 1) + "1"
 
+    return "".join(mirror(s))
+
+
+
+def main():
+    # data = Path("input.txt").read_text(encoding="utf-8")
+    # print(data, end="")
+    BASE_DIR = Path(__file__).resolve().parent
+    data = (BASE_DIR / "input.txt").read_text(encoding="utf-8")
+
+    numbers = data.splitlines()
+    for n in numbers:
+        print(next_magic_num(convert(n)))
 
 if __name__ == "__main__":
     main()
