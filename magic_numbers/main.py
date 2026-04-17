4ca5850ca2b30b60812ad0eb58964fa4cf5d5ddf
Andris <laczkovicsandris07.education@gmail.com>
2026-04-17 16:12:47 +0200

    n_m_n extra perfomance gain
    made some tweaks so it runs a little bit faster and maybe easier to read

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,44 +1,25 @@
 from pathlib import Path
 
-def convert(data: str) -> str:
-    total = 0
-
-    for line in data.splitlines():
-        line = line.strip()
-        if not line:
-            continue
-
-        # ha van ^, akkor hatvány
-        if "^" in line:
-            base, exp = line.split("^")
-            total += int(base) ** int(exp)
-        else:
-            # sima szám
-            digits = "".join(ch for ch in line if ch.isdigit())
-            if digits:
-                total += int(digits)
-
-    return str(total)
+def convert(n: str) -> str:
+    if "^" in n:
+        base, exp = n.split("^")
+        n = str(int(base) ** int(exp))
+    return n
 
 def next_magic_num(n: str) -> str:
     s = list(n)
-    length = len(s)
-
+    
     def mirror(arr):
-        arr = arr.copy()
-        l = len(arr)
-        for i in range(l // 2):
-            arr[l - 1 - i] = arr[i]
+        for i in range(len(arr) // 2):
+            arr[-1 - i] = arr[i]
         return arr
     
-    mirrored = mirror(s.copy())
-    if "".join(mirrored) > n:
-        return "".join(mirrored)
+    mirrored = "".join(mirror(s))
+    if mirrored > n:
+        return mirrored
     
     carry = 1
-    mid = (length - 1) // 2
-
-    i = mid
+    i = (len(s) - 1) // 2
     while i >= 0 and carry:
         new = int(s[i]) + carry
         carry = new // 10
@@ -46,20 +27,16 @@ def next_magic_num(n: str) -> str:
         i -= 1
 
     if carry:
-        return "1" + "0" * (length - 1) + "1"
+        return "1" + "0" * (len(s) - 1) + "1"
 
-    return "".join(mirror(s))
-
-
+    mirror(s)
+    return "".join(s)
 
 def main():
-    # data = Path("input.txt").read_text(encoding="utf-8")
-    # print(data, end="")
     BASE_DIR = Path(__file__).resolve().parent
     data = (BASE_DIR / "input.txt").read_text(encoding="utf-8")
 
-    numbers = data.splitlines()
-    for n in numbers:
+    for n in data.splitlines():
         print(next_magic_num(convert(n)))
 
 if __name__ == "__main__":
