acebea503b08f29b2d44092829906e08510c89a0
Andris <laczkovicsandris07.education@gmail.com>
2026-04-17 18:47:19 +0200

    parking_calculator solution
    After thinking it through I redesigned the code and deleted the useless methods

--- parking_calculator/main.py
+++ parking_calculator/main.py
@@ -1,66 +1,62 @@
 from pathlib import Path
-
 from datetime import datetime
 
+DAY = 86400
 HOUR = 3600
 MINUTE = 60
 
-def convert_to_total_seconds(START: str, END: str) -> int:
-    START_date = datetime.strptime(START, "%Y-%m-%d %H:%M:%S")
-    END_date = datetime.strptime(END, "%Y-%m-%d %H:%M:%S")
-    seconds = int((END_date - START_date).total_seconds())
+def convert_to_total_seconds(start: str, end: str) -> int:
+    startSeconds = datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
+    endSeconds = datetime.strptime(end, "%Y-%m-%d %H:%M:%S")
+    seconds = int((endSeconds - startSeconds).total_seconds())
 
     return seconds
 
-def convert_to_hours(seconds: int) -> int:
-    return seconds // HOUR
-
-def convert_to_minutes(seconds: int) -> int:
-    return seconds // MINUTE
-
-def remainder(seconds: int, base: int) -> int:
-    return seconds % base
-
-def breakdown(seconds: int):
-    hours = convert_to_hours(seconds)
-    remainder_after_hours = remainder(seconds, HOUR)
-
-    minutes = convert_to_minutes(remainder_after_hours)
-    remainder_after_minutes = remainder(remainder_after_hours, MINUTE)
-
-    return hours, minutes, remainder_after_minutes
-
-def parking_fee(START: str, END: str) -> int:
-    seconds = convert_to_total_seconds(START, END)
+def parking_fee(start: str, end: str) -> int:
+    seconds = convert_to_total_seconds(start, end)
 
     if seconds < 0:
         return -1
 
-    if seconds <= MINUTE * 30:
-        return 0
+    days = seconds // DAY
+    rest = seconds % DAY
 
-    return seconds
+    fee = days * 10000
+    freeParking = rest - 30 * MINUTE
 
-
+    if freeParking <= 0:
+        return fee
+    if freeParking <= 3 * HOUR:
+        fee += (freeParking + HOUR - 1) // HOUR * 300
+    else:
+        fee += 3 * 300
+        fee += ((freeParking - 3 * HOUR) + HOUR - 1) // HOUR * 500
+    
+    return fee
 
 def main():
-    # data = Path("input.txt").read_text(encoding="utf-8")
-    # print(data, end="")
-
     BASE_DIR = Path(__file__).resolve().parent
     data = (BASE_DIR / "input.txt").read_text(encoding="utf-8")
     
-    data = data.splitlines()
-    for line in data[2:]:
-        parts = line.split()
-        license_plate = parts[0]
-        start = parts[1] + " " + parts[2]
-        end = parts[3] + " " + parts[4]
-
-        if parking_fee(start, end) == -1:
-            print("A kilépési idő előbb volt, mint a belépési idő.")
+    with open("fees.txt", "w", encoding="utf-8") as f:
+        f.write("RENDSZÁM\tDÍJ\n")
         
-        print(parking_fee(start, end))
+        for line in data.splitlines()[2:]:
+            parts = line.split()
+
+            license_plate = parts[0]
+            start = parts[1] + " " + parts[2]
+            end = parts[3] + " " + parts[4]
+            
+            fee = parking_fee(start, end)
+
+            if fee == -1:
+                result = f"{license_plate}\t\tNem parkolt"
+            else:
+                result = f"{license_plate}\t\t{fee}"
+
+            print(result)
+            f.write(result + "\n")
 
 if __name__ == "__main__":
     main()
