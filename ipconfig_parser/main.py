598714572a1ff1dc16fadadab25f446cb01655ab
Andris <laczkovicsandris07.education@gmail.com>
2026-04-17 22:44:43 +0200

    ip_c decode improvements & print
    Improved the encode_any_file method and printed out the files that were collected from the folders. Discarded the changes to the .txt files so the encoding stays utf-16

--- ipconfig_parser/main.py
+++ ipconfig_parser/main.py
@@ -1,21 +1,35 @@
 from pathlib import Path
+import sys
+
+sys.stdout.reconfigure(encoding="utf-8")
+ENCODINGS = ["utf-8-sig","utf-8","utf-16","utf-16-le","utf-16-be","cp1250","cp1251","cp1252","cp1254","latin-1","iso-8859-2","iso-8859-1","iso-8859-15","cp850","cp852","cp437","mac_roman","koi8-r","shift_jis","gbk","utf-32"] # most common encodings
 
 def encode_any_file(path: Path, target: str = "utf-8"):
-    encodings = ["ascii","big5","big5hkscs","cp037","cp273","cp424","cp437","cp500","cp720","cp737","cp775","cp850","cp852","cp855","cp856","cp857","cp858","cp860","cp861","cp862","cp863","cp864","cp865","cp866","cp869","cp874","cp875","cp932","cp949","cp950","cp1006","cp1026","cp1125","cp1140","cp1250","cp1251","cp1252","cp1253","cp1254","cp1255","cp1256","cp1257","cp1258","euc_jp","euc_jis_2004","euc_jisx0213","euc_kr","gb2312","gbk","gb18030","hz","iso2022_jp","iso2022_jp_1","iso2022_jp_2","iso2022_jp_2004","iso2022_jp_3","iso2022_jp_ext","iso2022_kr","latin_1","iso8859_2","iso8859_3","iso8859_4","iso8859_5","iso8859_6","iso8859_7","iso8859_8","iso8859_9","iso8859_10","iso8859_11","iso8859_13","iso8859_14","iso8859_15","iso8859_16","johab","koi8_r","koi8_t","koi8_u","kz1048","mac_cyrillic","mac_greek","mac_iceland","mac_latin2","mac_roman","mac_turkish","ptcp154","shift_jis","shift_jis_2004","shift_jisx0213","utf_32","utf_32_be","utf_32_le","utf_16","utf_16_be","utf_16_le","utf_7","utf_8","utf_8_sig"]
-    # encodings were pasted from: https://stackoverflow.com/questions/1728376/get-a-list-of-all-the-encodings-python-can-encode-to
-
-    for enc in reversed(encodings):
+    for enc in ENCODINGS:
         try:
-            text = path.read_text(encoding=enc)
+            text = path.read_text(encoding=enc).replace("\ufeff", "")
             path.write_text(text, encoding=target)
             return enc
         except UnicodeDecodeError:
             pass
 
-    raise ValueError("Target Encode Type Was Not Found!")
+    raise ValueError("Decode failed!")
+
+def get_txt_files(base_dir: Path):
+    return list(base_dir.rglob("*.txt"))
 
 def main():
     BASE_DIR = Path(__file__).resolve().parent
+
+    files = get_txt_files(BASE_DIR)
+
+    for file in files:
+        enc = encode_any_file(file)
+
+        print(f"{file.name} -> {enc}")
+
+        text = file.read_text(encoding="utf-8", errors="replace")
+        print(text)
 
 if __name__ == "__main__":
     main()
